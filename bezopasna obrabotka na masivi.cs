53ad605cad28bf3f3085e7500f4ba494c3df1ba9
Vasilena Kolarova <35624769+VasilenaKolarova@users.noreply.github.com>
2018-01-28 09:16:21 +0200

    Add files via upload

---
+++ bezopasna obrabotka na masivi.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bezopasna_obrabotka_na_masivi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ').ToArray();
+            string[] inputDistinct = input;
+            bool found = false;
+            while (found == false)
+            {
+                string inputText = Console.ReadLine().ToLower();
+                if (inputText == "distinct")
+                {
+                    inputDistinct = input.Distinct();
+                    Console.WriteLine(string.Join(" ", input));
+                }
+
+                if (inputText == "reverse")
+                {
+                    Array.Reverse(input);
+                }
+
+                if (inputText == "end")
+                {
+                    found = true;
+                }
+            }
+        }
+    }
+}
