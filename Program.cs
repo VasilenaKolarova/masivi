0140ef912be17fdd791ea4059d079af0cd7a156a
Vasilena Kolarova <35624769+VasilenaKolarova@users.noreply.github.com>
2018-01-28 09:12:23 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sguni_i_suberi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int k = array.Length / 4;
+            
+            int br = 0;
+            for (int i = k; i < 2 * k; i++)
+            {
+                br = i; 
+            }
+            int[] firstNums = new int[array[k]];
+            
+            int[] lastNums = new int[array[array.Length-k]];
+            int[] midNums = new int[array[br]];
+            for (int j = 0; j < k - 1; j++)
+            {
+                 int[] sum1 = new int[firstNums[j] + midNums[j]];
+                Console.WriteLine(sum1);
+            }
+            for (int m = 0; m < k - 1; m++)
+            {
+                int[] sum2 = new int[lastNums[m] + midNums[m + 2 * k]];
+            }
+            
+
+        }
+    }
+}
---
+++ izvlichane na srednite 1, 2 ili 3  elementa.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace izvlicane_na_srednite_1__2_ili_3_elementa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int num1 = 0;
+            int num2 = 0;
+            int num3 = 0;
+            if (array.Length == 1)
+            {
+                Console.Write("{ ");
+                Console.Write("{0}", array[0]);
+                Console.WriteLine(" }");
+            }
+            else if (array.Length % 2 == 0)
+            {
+
+                num1 = array[array.Length / 2 - 1];
+                num2 = array[array.Length / 2];
+                Console.Write("{ ");
+                Console.Write("{0}, {1}", num1, num2);
+                Console.WriteLine(" }");
+            }
+            else if (array.Length % 2 != 0)
+            {
+                num1 = array[array.Length / 2 - 1];
+                num2 = array[array.Length / 2];
+                num3 = array[array.Length / 2 + 1];
+                Console.Write("{ ");
+                Console.Write("{0}, {1}, {2}", num1, num2, num3);
+                Console.WriteLine(" }");
+            }
+
+
+
+        }
+    }
+}
